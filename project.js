47ef03a6db8885d396f9aed349658076a4c97cdb
O-Will-O <winderswilliam122205@gmail.com>
2023-11-09 08:27:20 -0500

    add some js

---
+++ project.js
@@ -0,0 +1,19 @@
+let activePlayer = "X"
+let playerXScore = 0
+let playerOScore = 0
+const board = ['', '', '', '', '', '', '', '', '', ]
+const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+ ];
+
+let item = document.getElementsByClassName("grid-item")
+item.addEventListener("click", function add(){
+    alert("test";)
+})
